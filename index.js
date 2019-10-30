0b323a82d62aed2ab21d62e05da3c958d8eea477
tsgit99 <zezz513@gmail.com>
2019-10-30 15:11:01 +0700

    edit index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var app = express()
+var port = 80
 
 app.get('/', function (req, res){
 	res.send('hello world')
 })
+
+app.listen(port, () => console.log('app running on ' + port))
